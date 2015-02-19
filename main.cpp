1a0e4b80ef22bf8c2c8b946f117daa5af10f63de
GeoCook <geo_94@live.co.uk>
2015-02-19 21:18:50 +0000

    added vector class

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+
+ class Vector
+ {
+ public:
+   float m_x;
+   float m_y;
+   float m_z;
+ public:
+  Vector(float _x, float _y, float _z);
+ };
+
+
+  Vector::Vector(float _x, float _y, float _z)
+  {
+    m_x = _x;
+    m_y = _y;
+    m_z = _z;
+  }
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  Vector vec = Vector(5,6,7);
+  cout << "Hello world!" << endl;
+  cout << vec.m_x << endl;
+  return 0;
 }
